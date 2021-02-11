a2b1589343859c05bbf4772bebe67428a4a0008e
“William <william.alvaradoc@outlook.com>
2021-02-11 09:24:29 -0600

    Fully Functional API Connection

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using AzureSQLWebAPI.Models;
+
+namespace Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : Controller
+    {
+        private ContactsContext contactsContext;
+
+        public ContactController(ContactsContext context)
+        {
+            contactsContext = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Contacts>> Get()
+        {
+            return contactsContext.ContactsSet.ToList();
+        }
+
+        ~ContactController()
+        {
+            contactsContext.Dispose();
+        }
+
+
+    }
+}
