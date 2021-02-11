a6f41c5be98295ea394f5607975dc2e06da1641e
“William <william.alvaradoc@outlook.com>
2021-02-10 22:15:17 -0600

    This is my first commit for AzureSQLWebAPI

---
+++ Models/Contacts.cs
@@ -0,0 +1,10 @@
+namespace Models
+{
+    public class Contacts
+    {
+        public string identifier {get; set;}
+        public string name {get; set;}
+        public string email {get; set;}
+        public string phoneNumber {get; set;}
+    }
+}
